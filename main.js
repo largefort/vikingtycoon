a667c4eddd870a8244c96592d75920af47fc48e0
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-05-20 21:18:45 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,18 @@ import * as me from 'melonjs';
 import PlayScreen from './js/screens/play.js';
 
 me.device.onReady(function() {
+    // Initialize the video
     if (!me.video.init(1024, 768, { parent : "screen", scale : "auto", scaleMethod : "flex-width" })) {
         alert("Your browser does not support HTML5 canvas.");
         return;
     }
 
+    // Initialize the audio
     me.audio.init("mp3,ogg");
 
+    // Set the "Play/Ingame" Screen Object
     me.state.set(me.state.PLAY, new PlayScreen());
 
+    // Start the game.
     me.state.change(me.state.PLAY);
 });
